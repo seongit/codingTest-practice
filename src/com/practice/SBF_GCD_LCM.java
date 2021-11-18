212c18d02a8a793056053652bc7a647ca8c7f46e
seongeun <seongeunk102@gmail.com>
2021-11-19 07:03:11 +0900

    Type Algorithm GCB_LCM

---
+++ src/com/practice/SBF_GCD_LCM.java
@@ -0,0 +1,34 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class SBF_GCD_LCM {
+
+    public static void main(String[]args){
+
+        // 두 수를 입력 받는다.
+        // 입력 받은 두 수를 i로 나눈다. (i의 값은 2부터 시작한다.)
+        // 나누어 떨어질 경우 몫을 외부 변수에 저장한다
+        // 외부 변수(1)과 외부변수(2)의 값이 같을 경우 최종으로 해당 결과값을 저장한다.
+        // i가 증가된다.
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result1 = 0;
+        int result2 = 0;
+        int answer = 0;
+
+        for(int i=1;i<10;i++){
+            result1=a/i;
+            result2=b/i;
+            //System.out.println(result1);
+            System.out.println(result2);
+            if(result1==result2){
+                answer = result1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
